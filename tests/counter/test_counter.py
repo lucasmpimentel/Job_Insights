077bde3d655d1a02541a62de050f1a453420689d
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-09-21 01:55:56 -0300

    req 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = 'src/jobs.csv'
+    word = 'python'
+    result = 1639
+    assert count_ocurrences(path, word) == result
